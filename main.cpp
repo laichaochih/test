5c63144c947ad3d8080522ea2365a5cd52449d5d
laichaochih <126858479+laichaochih@users.noreply.github.com>
2023-03-04 05:25:18 +0000

    change the word

--- main.cpp
+++ main.cpp
@@ -2,30 +2,12 @@
 
 using namespace std;
 
-int step(int n, char c)
-{
-    for(int x = 1; x <= n; x++)
-    {
-        if(x == 1)
-        {
-            if(hb[0] == 0)
-            {
-
-                step(x)
-            }
-        }
-    }
-
-}
-
 int main()
 {
-    cin >> n;
-    int ha[n] = {0}, hb[n] = {0}, hc[n] = {0};
-    for(int k = 0; k < 3; k++)
+    cout << "nothing" << endl;
+    for(int x = 0 ; x < 10 ; x++)
     {
-        ha[k] = n;
+        cout << x << endl;
     }
-    step(n, 'a')
     return 0;
 }
